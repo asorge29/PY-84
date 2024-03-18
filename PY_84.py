a2d80b533b777c03465de2bb387aa7493b553db7
BassMaster629 <oceans4496@gmail.com>
2024-03-18 13:36:57 -0700

    seyupui and signals

--- PY_84.py
+++ PY_84.py
@@ -5,4 +5,14 @@ from ti84_window import Ui_py84_Window
 class PY84(QWidget, Ui_py84_Window):
     def __init__(self):
         super().__init__()
+        self._setupUI()
+        self.currentEquation = ''
+
+    def _setupUI(self):
         self.setupUi(self)
+
+    def _connectButtonSignalsNormal(self):
+        pass
+
+    def _insertNumber(self, number, index=int):
+        pass
